64126b307704329666fcaa8bcc33845e05da440e
강나연 <skdusdl4@naver.com>
2024-12-19 15:24:59 +0900

    [add] index 파일 아이템 이미지 추가

--- public/index.js
+++ public/index.js
@@ -40,6 +40,8 @@ const ITEM_CONFIG = [
   { width: 50 / 1.5, height: 50 / 1.5, id: 2, image: 'images/items/pokeball_yellow.png' },
   { width: 50 / 1.5, height: 50 / 1.5, id: 3, image: 'images/items/pokeball_purple.png' },
   { width: 50 / 1.5, height: 50 / 1.5, id: 4, image: 'images/items/pokeball_cyan.png' },
+  { width: 50 / 1.5, height: 50 / 1.5, id: 5, image: 'images/items/pokeball_pink.png' },
+  { width: 50 / 1.5, height: 50 / 1.5, id: 6, image: 'images/items/pokeball_orange.png' },
 ];
 
 // 게임 요소 초기화
